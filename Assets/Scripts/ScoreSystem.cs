dcb2df957d65b706ce91cdaafa2a3361b392b6f2
Mikita Slabysh <slabysh2015@gmail.com>
2021-12-10 13:35:47 -0500

    Score

---
+++ Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreSystem : MonoBehaviour
+{
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private float scoreMultiplier;
+
+    private float score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score += Time.deltaTime * scoreMultiplier;
+
+        scoreText.text = Mathf.FloorToInt(score).ToString();//Round down the value
+    }
+}
