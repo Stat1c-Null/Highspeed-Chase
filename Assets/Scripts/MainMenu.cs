81509cb4535557b7bccc41aa2a6371bffcf26e31
Mikita Slabysh <slabysh2015@gmail.com>
2021-12-13 13:56:34 -0500

    Added energy and energy recharge into the game

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private TMP_Text highScoreText;
+    [SerializeField] private TMP_Text energyText;
+    [SerializeField] private int maxEnergy;
+    [SerializeField] private int energyRecharge;
+
+    private int energy;
+
+    private const string EnergyKey = "Energy";
+    private const string EnergyReadyKey = "EnergyReady";
+
+    private void Start()
+    {
+        int highScore = PlayerPrefs.GetInt(ScoreSystem.HighScoreKey, 0);
+        highScoreText.text = $"High Score: {highScore}";
+        //Let player play only if they have energy
+        energy = PlayerPrefs.GetInt(EnergyKey, maxEnergy);//Check if we have enough energy
+        //Check if player is out of energy
+        if(energy == 0)
+        {
+            string energyReadyString = PlayerPrefs.GetString(EnergyReadyKey, string.Empty);//Check if energy should be regenerated
+
+            if(energyReadyString == string.Empty) { return; }
+
+            System.DateTime energyReady = System.DateTime.Parse(energyReadyString);//Convert from string into DateTime object
+
+            if(System.DateTime.Now > energyReady)//Check if enough time passed to regenerate
+            {
+                energy = maxEnergy;
+                PlayerPrefs.SetInt(EnergyKey, energy);
+            }
+        }
+        energyText.text = $"Energy Left : {energy} ";
+    }
+
+    public void Play()
+    {
+        //Consume energy when play the game
+        if(energy < 1) { return; }
+        energy -= 1;
+        PlayerPrefs.SetInt(EnergyKey, energy);
+
+        if(energy < maxEnergy)//Check if energy is less than max so regeneration can start
+        {
+            System.DateTime energyReady = System.DateTime.Now.AddMinutes(energyRecharge);
+            PlayerPrefs.SetString(EnergyReadyKey, energyReady.ToString());
+        }
+
+        SceneManager.LoadScene(1);
+    }
+}
--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -8,6 +8,8 @@ public class ScoreSystem : MonoBehaviour
     [SerializeField] private TMP_Text scoreText;
     [SerializeField] private float scoreMultiplier;
 
+    public const string HighScoreKey = "HighScore";
+
     private float score;
     // Start is called before the first frame update
     void Start()
@@ -21,5 +23,15 @@ public class ScoreSystem : MonoBehaviour
         score += Time.deltaTime * scoreMultiplier;
 
         scoreText.text = Mathf.FloorToInt(score).ToString();//Round down the value
+    }
+
+    private void OnDestroy()
+    {
+        //Save player's highscore
+        int currentHighScore = PlayerPrefs.GetInt(HighScoreKey, 0);
+        if(score > currentHighScore)
+        {
+            PlayerPrefs.SetInt(HighScoreKey, Mathf.FloorToInt(score));
+        }
     }
 }
